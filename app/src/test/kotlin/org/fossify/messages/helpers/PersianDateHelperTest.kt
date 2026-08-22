1b3b1894ef1b714289344b3dca4deff1be71bcc3
freetux <thefreetux@gmail.com>
2026-08-22 09:40:26 +0330

    test: cover localized Jalali date output

--- app/src/test/kotlin/org/fossify/messages/helpers/PersianDateHelperTest.kt
+++ app/src/test/kotlin/org/fossify/messages/helpers/PersianDateHelperTest.kt
@@ -3,6 +3,7 @@ package org.fossify.messages.helpers
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.time.Instant
+import java.util.Locale
 
 class PersianDateHelperTest {
     @Test
@@ -14,5 +15,17 @@ class PersianDateHelperTest {
     @Test
     fun convertsDigitsToPersianDigits() {
         assertEquals("۱۴۰۵/۱/۱", PersianDateHelper.toPersianDigits("1405/1/1"))
+    }
+
+    @Test
+    fun formatsEnglishMonthAbbreviationWithLatinDigits() {
+        val timestamp = Instant.parse("2026-08-21T00:00:00Z").toEpochMilli()
+        assertEquals("30 mor 1405", PersianDateHelper.formatMonthName(timestamp, Locale.ENGLISH))
+    }
+
+    @Test
+    fun formatsPersianMonthNameWithPersianDigits() {
+        val timestamp = Instant.parse("2026-08-21T00:00:00Z").toEpochMilli()
+        assertEquals("۳۰ مرداد ۱۴۰۵", PersianDateHelper.formatMonthName(timestamp, Locale("fa")))
     }
 }
