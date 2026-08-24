d5afa7f9053d30807914b4be5944ca93ec16985b
freetux <thefreetux@gmail.com>
2026-08-24 13:52:16 +0330

    fix: reliably launch advanced search activity

--- app/src/main/kotlin/org/fossify/messages/views/AdvancedSearchLauncherView.kt
+++ app/src/main/kotlin/org/fossify/messages/views/AdvancedSearchLauncherView.kt
@@ -3,6 +3,7 @@ package org.fossify.messages.views
 import android.content.Context
 import android.content.Intent
 import android.util.AttributeSet
+import android.widget.Toast
 import org.fossify.commons.views.MyTextView
 import org.fossify.messages.activities.AdvancedSearchActivity
 
@@ -16,7 +17,19 @@ class AdvancedSearchLauncherView @JvmOverloads constructor(
         isClickable = true
         isFocusable = true
         setOnClickListener {
-            context.startActivity(Intent(context, AdvancedSearchActivity::class.java))
+            try {
+                val intent = Intent(context, AdvancedSearchActivity::class.java).apply {
+                    addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                }
+                context.startActivity(intent)
+            } catch (e: Exception) {
+                Toast.makeText(context, e.message ?: "Unable to open Advanced Search", Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    override fun performClick(): Boolean {
+        super.performClick()
+        return true
     }
 }
