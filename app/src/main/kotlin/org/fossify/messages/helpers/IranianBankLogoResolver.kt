c19380d4f2a0d2781e49928eae9790ec82b69aea
freetux <thefreetux@gmail.com>
2026-08-26 08:44:16 +0330

    debug: trace Sepah drawable resolution

--- app/src/main/kotlin/org/fossify/messages/helpers/IranianBankLogoResolver.kt
+++ app/src/main/kotlin/org/fossify/messages/helpers/IranianBankLogoResolver.kt
@@ -7,8 +7,21 @@ import androidx.annotation.DrawableRes
 object IranianBankLogoResolver {
     @DrawableRes
     fun resolve(context: Context, bank: IranianBankRegistry.BankInfo): Int? {
-        val resourceName = bank.logoResourceName ?: return null
+        val resourceName = bank.logoResourceName
+        if (bank.id == IranianBankRegistry.BankId.SEPAH) {
+            DebugLog.write(context, "SEPAH_UI_RESOLVE_START bankId=${bank.id} resourceName=$resourceName package=${context.packageName}")
+        }
+        if (resourceName == null) {
+            if (bank.id == IranianBankRegistry.BankId.SEPAH) {
+                DebugLog.write(context, "SEPAH_UI_RESOLVE_NO_RESOURCE_NAME")
+            }
+            return null
+        }
+
         val resourceId = context.resources.getIdentifier(resourceName, "drawable", context.packageName)
+        if (bank.id == IranianBankRegistry.BankId.SEPAH) {
+            DebugLog.write(context, "SEPAH_UI_RESOLVE_RESULT resourceName=$resourceName resourceId=$resourceId found=${resourceId != 0}")
+        }
         return resourceId.takeIf { it != 0 }
     }
 }
