df17b9bc629f1ebf48c2212b5d91d3f043235e60
freetux <thefreetux@gmail.com>
2026-08-28 11:21:53 +0330

    fix: declare shared library plugins at root

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
 plugins {
     alias(libs.plugins.android).apply(false)
+    alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.kotlinSerialization).apply(false)
     alias(libs.plugins.ksp).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
+    alias(libs.plugins.parcelize).apply(false)
     alias(libs.plugins.detekt).apply(false)
 }
 
