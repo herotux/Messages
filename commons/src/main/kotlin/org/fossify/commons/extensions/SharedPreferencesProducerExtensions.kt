2a4061c98b52cad0f8deaf704ee6510b4f1c3937
freetux <thefreetux@gmail.com>
2026-08-28 11:36:53 +0330

    fix: replace obsolete Commons context receiver

--- commons/src/main/kotlin/org/fossify/commons/extensions/SharedPreferencesProducerExtensions.kt
+++ commons/src/main/kotlin/org/fossify/commons/extensions/SharedPreferencesProducerExtensions.kt
@@ -5,19 +5,21 @@ import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 
-context (SharedPreferences)
-fun <T> sharedPreferencesCallback(
+fun <T> SharedPreferences.sharedPreferencesCallback(
     sendOnCollect: Boolean = false,
-    value: () -> T?,
+    value: SharedPreferences.() -> T?,
 ): Flow<T?> = callbackFlow {
     val sharedPreferencesListener =
         SharedPreferences.OnSharedPreferenceChangeListener { _, _ ->
-            trySend(value())
+            trySend(value(this@sharedPreferencesCallback))
         }
 
     if (sendOnCollect) {
-        trySend(value())
+        trySend(value(this@sharedPreferencesCallback))
     }
+
     registerOnSharedPreferenceChangeListener(sharedPreferencesListener)
-    awaitClose { unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener) }
+    awaitClose {
+        unregisterOnSharedPreferenceChangeListener(sharedPreferencesListener)
+    }
 }
