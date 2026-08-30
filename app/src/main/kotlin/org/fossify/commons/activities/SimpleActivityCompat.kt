856f9bb2bcf9c65937d58fabc7a0a24a00392f5e
freetux <thefreetux@gmail.com>
2026-08-30 15:50:19 +0330

    fix: restore SimpleActivity compatibility alias

---
+++ app/src/main/kotlin/org/fossify/commons/activities/SimpleActivityCompat.kt
@@ -0,0 +1,7 @@
+package org.fossify.commons.activities
+
+/**
+ * Compatibility alias for older feature code while Fossify Commons exposes
+ * BaseSimpleActivity as the public base activity.
+ */
+typealias SimpleActivity = BaseSimpleActivity
