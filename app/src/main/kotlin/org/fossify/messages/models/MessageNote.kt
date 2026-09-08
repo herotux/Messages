20eb4019e64292b5a443ab2bc249b5016c771a82
freetux <thefreetux@gmail.com>
2026-09-08 11:54:16 +0330

    fix: map message note primary key column for Room

--- app/src/main/kotlin/org/fossify/messages/models/MessageNote.kt
+++ app/src/main/kotlin/org/fossify/messages/models/MessageNote.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "message_notes")
 data class MessageNote(
-    @PrimaryKey val messageId: Long,
+    @PrimaryKey @ColumnInfo(name = "message_id") val messageId: Long,
     @ColumnInfo(name = "text") val text: String,
     @ColumnInfo(name = "created_at") val createdAt: Long,
     @ColumnInfo(name = "updated_at") val updatedAt: Long,
