4edaa2f40e3adc2a5da2a83b17089fea3bf05a17
freetux <thefreetux@gmail.com>
2026-09-08 12:36:29 +0330

    fix: import annotation DAO

--- app/src/main/kotlin/org/fossify/messages/helpers/MessageAnnotationStore.kt
+++ app/src/main/kotlin/org/fossify/messages/helpers/MessageAnnotationStore.kt
@@ -3,6 +3,7 @@ package org.fossify.messages.helpers
 import android.content.Context
 import org.fossify.commons.extensions.getProperPrimaryColor
 import org.fossify.messages.extensions.messagesDB
+import org.fossify.messages.interfaces.AnnotationLabelsDao
 import org.fossify.messages.models.AnnotationLabel
 import org.fossify.messages.models.ConversationLabel
 import org.fossify.messages.models.ConversationNote
@@ -10,7 +11,7 @@ import org.fossify.messages.models.MessageLabel
 import org.fossify.messages.models.MessageNote
 
 object MessageAnnotationStore {
-    private fun dao(context: Context) = context.messagesDB.AnnotationLabelsDao()
+    private fun dao(context: Context): AnnotationLabelsDao = context.messagesDB.AnnotationLabelsDao()
 
     fun getMessageLabels(context: Context, messageId: Long): List<AnnotationLabel> = dao(context).getMessageLabels(messageId)
     fun getMessageNote(context: Context, messageId: Long): MessageNote? = dao(context).getMessageNote(messageId)
