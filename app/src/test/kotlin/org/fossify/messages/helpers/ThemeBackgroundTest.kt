61e9d8962d1aa2642b141c7c4c33b93cb91e0fee
freetux <thefreetux@gmail.com>
2026-09-11 13:06:30 +0330

    test: cover theme background angle normalization

---
+++ app/src/test/kotlin/org/fossify/messages/helpers/ThemeBackgroundTest.kt
@@ -0,0 +1,16 @@
+package org.fossify.messages.helpers
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class ThemeBackgroundTest {
+    @Test
+    fun normalizeAngle_snapsToNearest45Degrees() {
+        assertEquals(0, ThemeBackground.normalizeAngle(0))
+        assertEquals(45, ThemeBackground.normalizeAngle(22))
+        assertEquals(45, ThemeBackground.normalizeAngle(23))
+        assertEquals(315, ThemeBackground.normalizeAngle(-46))
+        assertEquals(0, ThemeBackground.normalizeAngle(360))
+        assertEquals(315, ThemeBackground.normalizeAngle(359))
+    }
+}
