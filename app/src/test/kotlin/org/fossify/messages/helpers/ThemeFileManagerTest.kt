33b0194c7c5201fbb22ec0eb74a8fb6f27c03193
freetux <thefreetux@gmail.com>
2026-09-11 15:05:25 +0330

    fix: correct embedded wallpaper import test fixture

--- app/src/test/kotlin/org/fossify/messages/helpers/ThemeFileManagerTest.kt
+++ app/src/test/kotlin/org/fossify/messages/helpers/ThemeFileManagerTest.kt
@@ -65,8 +65,8 @@ class ThemeFileManagerTest {
             "\"backgroundType\": \"SOLID\"",
             "\"backgroundType\": \"WALLPAPER\""
         ).replace(
-            "\"wallpaperUri\": null",
-            "\"wallpaperUri\": null,\n      \"wallpaperBase64\": \"aGVsbG8=\""
+            "\"wallpaperBase64\": null",
+            "\"wallpaperBase64\": \"aGVsbG8=\""
         )
         val imported = ThemeFileManager.importTheme(raw).getOrThrow()
         assertEquals(ThemeManager.BackgroundType.WALLPAPER, imported.backgroundType)
