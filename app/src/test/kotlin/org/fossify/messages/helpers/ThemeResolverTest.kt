2981e5d7c7e5ec33e54b94426c593030353f20ab
freetux <thefreetux@gmail.com>
2026-09-13 22:25:03 +0330

    test(theme): use JUnit assertions in ThemeResolverTest (#26)

--- app/src/test/kotlin/org/fossify/messages/helpers/ThemeResolverTest.kt
+++ app/src/test/kotlin/org/fossify/messages/helpers/ThemeResolverTest.kt
@@ -1,7 +1,7 @@
 package org.fossify.messages.helpers
 
-import kotlin.test.Test
-import kotlin.test.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
 class ThemeResolverTest {
     private val colors = ThemeManager.ThemeColors(
