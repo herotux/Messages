2aa6a676d9958d0461690fad8a2112ef848c63ce
freetux <thefreetux@gmail.com>
2026-09-17 08:48:28 +0330

    restore activity compatibility ownership

--- app/src/main/kotlin/org/fossify/messages/activities/SimpleActivity.kt
+++ app/src/main/kotlin/org/fossify/messages/activities/SimpleActivity.kt
@@ -1,6 +1,5 @@
 package org.fossify.messages.activities
 
-import android.content.Intent
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.View
@@ -39,9 +38,14 @@ open class SimpleActivity : BaseSimpleActivity() {
         appliedFontSize = config.fontSize
     }
 
-    private fun applyVisualTheme() { ThemeApplier.apply(this) }
+    private fun applyVisualTheme() {
+        ThemeApplier.apply(this)
+    }
 
-    private fun applyLocaleLayoutDirection() { window.decorView.layoutDirection = resources.configuration.layoutDirection }
+    private fun applyLocaleLayoutDirection() {
+        val direction = resources.configuration.layoutDirection
+        window.decorView.layoutDirection = direction
+    }
 
     private fun applySelectedFontToViewTree(view: View) {
         val typeface = runCatching { FontHelper.getTypeface(this) }.getOrElse { Typeface.DEFAULT }
@@ -58,26 +62,33 @@ open class SimpleActivity : BaseSimpleActivity() {
             }
             view.setTypeface(typeface, currentStyle)
         }
-        if (view is ViewGroup) for (index in 0 until view.childCount) applyTypeface(view.getChildAt(index), typeface)
+        if (view is ViewGroup) {
+            for (index in 0 until view.childCount) applyTypeface(view.getChildAt(index), typeface)
+        }
     }
 
     override fun getAppIconIDs() = arrayListOf(
-        R.mipmap.ic_launcher_red, R.mipmap.ic_launcher_pink, R.mipmap.ic_launcher_purple,
-        R.mipmap.ic_launcher_deep_purple, R.mipmap.ic_launcher_indigo, R.mipmap.ic_launcher_blue,
-        R.mipmap.ic_launcher_light_blue, R.mipmap.ic_launcher_cyan, R.mipmap.ic_launcher_teal,
-        R.mipmap.ic_launcher, R.mipmap.ic_launcher_light_green, R.mipmap.ic_launcher_lime,
-        R.mipmap.ic_launcher_yellow, R.mipmap.ic_launcher_amber, R.mipmap.ic_launcher_orange,
-        R.mipmap.ic_launcher_deep_orange, R.mipmap.ic_launcher_brown, R.mipmap.ic_launcher_blue_grey,
+        R.mipmap.ic_launcher_red,
+        R.mipmap.ic_launcher_pink,
+        R.mipmap.ic_launcher_purple,
+        R.mipmap.ic_launcher_deep_purple,
+        R.mipmap.ic_launcher_indigo,
+        R.mipmap.ic_launcher_blue,
+        R.mipmap.ic_launcher_light_blue,
+        R.mipmap.ic_launcher_cyan,
+        R.mipmap.ic_launcher_teal,
+        R.mipmap.ic_launcher,
+        R.mipmap.ic_launcher_light_green,
+        R.mipmap.ic_launcher_lime,
+        R.mipmap.ic_launcher_yellow,
+        R.mipmap.ic_launcher_amber,
+        R.mipmap.ic_launcher_orange,
+        R.mipmap.ic_launcher_deep_orange,
+        R.mipmap.ic_launcher_brown,
+        R.mipmap.ic_launcher_blue_grey,
         R.mipmap.ic_launcher_grey_black
     )
 
     override fun getAppLauncherName() = getString(R.string.app_launcher_name)
     override fun getRepositoryName() = "Messages"
-
-    /** Compatibility helpers previously supplied by the external Fossify Commons artifact. */
-    fun launchRecycleBin() = startActivity(Intent(this, RecycleBinConversationsActivity::class.java))
-    fun launchArchivedConversations() = startActivity(Intent(this, ArchivedConversationsActivity::class.java))
-    fun launchSettings() = startActivity(Intent(this, SettingsActivity::class.java))
-    fun launchAbout() = startActivity(Intent(this, HerotuxAboutActivity::class.java))
-    fun checkWhatsNewDialog() = runCatching { checkWhatsNew() }
 }
